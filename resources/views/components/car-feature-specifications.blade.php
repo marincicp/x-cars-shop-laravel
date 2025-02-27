067e3165341156c8d16f503836c301b54cdb86f3
Petar <marincicpetar@gmail.com>
2025-02-27 20:00:11 +0100

    carf feature components added

---
+++ resources/views/components/car-feature-specifications.blade.php
@@ -0,0 +1,30 @@
+@props(['car'])
+<ul class="car-specifications">
+    <x-car-feature-specification-item :value="$car->features->abs">ABS</x-car-feature-specification-item>
+
+    <x-car-feature-specification-item :value="$car->features->power_windows"> Power Windows</x-car-feature-specification-item>
+
+    <x-car-feature-specification-item :value="$car->features->power_door_lock"> Power Door Locks</x-car-feature-specification-item>
+
+    <x-car-feature-specification-item :value="$car->features->cruise_control">
+        Cruise Control</x-car-feature-specification-item>
+
+    <x-car-feature-specification-item :value="$car->features->remote_start">
+        Remote Start</x-car-feature-specification-item>
+
+    <x-car-feature-specification-item :value="$car->features->gps_navigation">
+        GPS Navigation System</x-car-feature-specification-item>
+
+    <x-car-feature-specification-item :value="$car->features->heated_seats">
+        Heated Seats</x-car-feature-specification-item>
+
+    <x-car-feature-specification-item :value="$car->features->climate_control">
+        Climate Control</x-car-feature-specification-item>
+
+    <x-car-feature-specification-item :value="$car->features->rear_parking_sensors">
+        Rear Parking Sensors</x-car-feature-specification-item>
+
+    <x-car-feature-specification-item :value="$car->features->leather_seats">
+        Leather Seats</x-car-feature-specification-item>
+
+</ul>
