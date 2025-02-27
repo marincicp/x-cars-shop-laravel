aa396c285d5ec65a1359925a3b39ac10e5031051
Petar <marincicpetar@gmail.com>
2025-02-27 19:53:12 +0100

    controllers new changes and n1 problem fixed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Car;
+use App\Models\FuelType;
+use App\Models\Maker;
+use App\Models\Model;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Mockery\Matcher\Not;
 
 class HomeController extends Controller
 {
 
     public function index()
     {
-        return view("home.index");
+        $cars = Car::with(["carType", "fuelType", "maker", "model",  "city", "primaryImage"])->where("published_at", "<", now())->orderBy("published_at", "desc")->limit(39)->get();
+
+        return view("home.index", ["cars" => $cars]);
     }
 }
