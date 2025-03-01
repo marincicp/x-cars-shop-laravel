92b7b26a512e7ab73be90ec271f8f3e85bd4b5c6
Petar <marincicpetar@gmail.com>
2025-03-01 21:16:29 +0100

    added api controller for city and model

---
+++ app/Http/Controllers/Api/CityApiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\State;
+
+class CityApiController extends Controller
+{
+
+    /**
+     * Get all citites by state
+     */
+    public function getCitiesByState(string $id)
+    {
+        $state = State::with("cities")->find($id);
+
+        if (!$state) {
+            return response()->json(["message" => "State not found"], 404);
+        }
+
+        return response()->json($state->cities);
+    }
+}
