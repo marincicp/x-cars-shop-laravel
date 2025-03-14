8b7d0823294b9b1c397c2178ed100e6ba3d57e8d
Petar <marincicpetar@gmail.com>
2025-03-14 14:31:10 +0100

    home page fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,10 +16,12 @@ class HomeController extends Controller
 
     public  function index()
     {
-
+        $favoriteCars = [];
         $cars = Car::with(["carType", "fuelType", "maker", "model",  "city", "primaryImage"])->where("published_at", "<", now())->orderBy("published_at", "desc")->paginate(15);
 
-        $favoriteCars = Auth::user()->favoriteCars()->pluck("car_id")->toArray();
+        if (Auth::check()) {
+            $favoriteCars = Auth::user()->favoriteCars()->pluck("car_id")->toArray();
+        }
 
         return View("home.index",  array_merge(["cars" => $cars, "favCars" => $favoriteCars], $this->dropdownCachedData));
     }
