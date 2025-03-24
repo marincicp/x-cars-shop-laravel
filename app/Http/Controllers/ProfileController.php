f9d9fd55adf0b1718b61cf285dc4759e00debe26
Petar <marincicpetar@gmail.com>
2025-03-24 13:14:58 +0100

    Profile controller added

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Repositories\UserRepository;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+
+class ProfileController extends Controller
+{
+
+
+    public function __construct(protected UserRepository $UserRepo) {}
+
+    /**
+     * Show edit user form
+     * @param \App\Models\User $user
+     * @return View
+     */
+    public function edit(User $user): View
+    {
+        return view("profile.edit", data: ["user" => $user]);
+    }
+
+
+    /**
+     * Update user data
+     * @param \App\Http\Requests\UpdateUserRequest $request
+     * @param \App\Models\User $user
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(UpdateUserRequest $request, User $user): RedirectResponse
+    {
+        $validateData = $request->validated();
+
+        $res = $this->UserRepo->updateCurrentUser($user, $validateData);
+
+        if (!$res) {
+            return back()->with(["message.error" => "Failed to update user data"]);
+        }
+
+        return back()->with(["message.success" => "User data updated successfully"]);
+    }
+}
