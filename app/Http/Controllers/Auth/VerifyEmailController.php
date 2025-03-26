243b49485f78a221ac719a69f10a4c8a55d64786
Petar <marincicpetar@gmail.com>
2025-03-26 22:14:31 +0100

    added VerifyEmailController

---
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\RedirectResponse;
+
+class VerifyEmailController extends Controller
+{
+
+
+    /**
+     * Mark the authenticated user's email address as verified
+     */
+    public function __invoke(EmailVerificationRequest $request): RedirectResponse
+    {
+        $request->fulfill();
+        return to_route("home");
+    }
+}
