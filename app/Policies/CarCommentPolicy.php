47669f02e6d70eafa9d5432bccd1402bfd06e0d7
Petar <marincicpetar@gmail.com>
2025-04-18 21:58:49 +0200

    car comment policy

---
+++ app/Policies/CarCommentPolicy.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\CarComment;
+use App\Models\User;
+
+class CarCommentPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+
+
+    public function delete(User $user, CarComment $comment): bool
+    {
+        return $user->id === $comment->user_id;
+    }
+}
